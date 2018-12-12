0d68e0a3656b33e8823b587ccc231186c115a1fe
cesare camurani <cesare.camurani@gmail.com>
2018-12-12 10:12:47 +0000

    Change note color - Max lenght 20 chars

--- app.js
+++ app.js
@@ -1,7 +1,5 @@
 window.onload=function() {
-
   document.getElementById("post").addEventListener("click", function() {
-    document.getElementById("notes").innerHTML += displayNote()
+    document.getElementById("notes").innerHTML += displayNote();
   });
-
 }
--- js/note.js
+++ js/note.js
@@ -1,4 +1,9 @@
 function displayNote() {
-  return document.getElementById("note").value + "<br>"
+  var newNote = document.getElementById("note").value + "<br>"
+  return newNote.fontcolor(randomColor());
+}
 
+function randomColor() {
+  var colors = ['red', 'green', 'blue', 'orange', 'yellow'];
+  return colors[Math.floor(Math.random() * colors.length)];
 }
